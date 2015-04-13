00bc08a935a14429afc2c35627dcb7a65cd83e6c
martabajo <martabg2492@gmail.com>
2015-04-13 09:58:17 +0200

    comando eat

--- Game.java
+++ Game.java
@@ -189,7 +189,7 @@ public class Game
         printLocationInfo();
     }
     
-    private void eat()
+    public void eat()
     {
        System.out.println("You have eaten now and you are not hungry any more");
     }
